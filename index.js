c71d8d6489f21bcf44eff6e5ab604dd102cb46aa
ndfe <trdvbxz@proton.me>
2023-04-09 12:06:20 +0000

    Update 13 files

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// const port = process.env.PORT || 8080;
+const port = 8080;
 const { execSync } = require("child_process");
 
-execSync(`chmod +x ./start.sh && ./start.sh`,{
-	cwd: './'
-})
+execSync(`chmod +x ./start.sh && ./start.sh`, {
+  cwd: "./",
+});
